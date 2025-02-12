02d0ec84b3ba6caa4184af2b51e9ed4b595e2d4f
ThiboFranssenPXL <Thibo.Franssen@student.pxl.be>
2025-02-12 16:46:09 +0100

    init

---
+++ voorbeelden/Example2/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Example2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            outputLabel.Content = "Hello 1TINa";
+            Debug.WriteLine("Dit is een tekst");
+        }
+
+        private void messageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello 1TINa");
+        }
+    }
+}
